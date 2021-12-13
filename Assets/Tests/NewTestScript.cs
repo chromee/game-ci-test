2d90c24e87c4ad09b57bad1a30efc6d702c09ece
chromee <odk.mmmm@gmail.com>
2021-12-13 15:08:43 +0900

    unity test

---
+++ Assets/Tests/NewTestScript.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        [Test]
+        public void Success()
+        {
+            Debug.Log("Hoge");
+        }
+
+        [Test]
+        public void Failed()
+        {
+            Debug.Log("Fuga");
+            Assert.IsTrue(false);
+        }
+    }
+}
